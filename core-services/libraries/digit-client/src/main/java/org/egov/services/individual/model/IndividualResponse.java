19594469a8a526e4a8651ae26dca212e3928fdb1
srujana-egov <srujana.dadi@egovernments.org>
2026-05-15 05:15:38 +0530

    Add @JsonProperty("Individuals") to the individual field

--- core-services/libraries/digit-client/src/main/java/org/egov/services/individual/model/IndividualResponse.java
+++ core-services/libraries/digit-client/src/main/java/org/egov/services/individual/model/IndividualResponse.java
@@ -13,6 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class IndividualResponse {
-    @JsonProperty(value="Individual")
+    @JsonProperty("Individuals")
     private Individual individual;
 }
